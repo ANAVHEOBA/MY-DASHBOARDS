08205fe632a5fb282ef365598bbbf7ba0114eb80
Victory Ifebhor <victory.ifebhor@gmail.com>
2025-01-25 20:15:52 +0100

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://ready-back-end.onrender.com';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const API_ENDPOINTS = {
   sessions: {
